a246bb261cd19830468dd07497ef81db5308dd5a
Mia Pars Markussen <miax7855@edu.eal.dk>
2019-02-26 22:24:38 +0100

    find customer gui lavet

--- Pretty hair/New Order.xaml.cs
+++ Pretty hair/New Order.xaml.cs
@@ -32,5 +32,37 @@ namespace Pretty_hair
             TextBox textBox = sender as TextBox;
             textBox.Text = "";
         }
+
+        private void Find_Customer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Find_Customer_Textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Image img = ((Image)sender);
+            // Height="86" Width="109"
+
+            img.Height = img.Height / 1.1;
+            img.Width = img.Height / 1.1;
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image img = ((Image)sender);
+            // Height="86" Width="109"
+            img.Height = img.Height * 1.1;
+            img.Width = img.Height * 1.1;
+        }
     }
 }
